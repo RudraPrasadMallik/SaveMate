f57deaee3757efae29d6ffd37b1815f1bc0546b8
Rudra Prasad Mallik <117834825+RudraPrasadMallik@users.noreply.github.com>
2025-03-24 19:35:42 +0530

    Removed Config Server

--- src/main/java/com/savemate/SaveMateApplication.java
+++ src/main/java/com/savemate/SaveMateApplication.java
@@ -2,16 +2,16 @@ package com.savemate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+//import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
+//@EnableConfigServer
 public class SaveMateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaveMateApplication.class, args);
 		
-//		 BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//	 BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 //	        String rawPassword = "admin123";
 //	        String encodedPassword = encoder.encode(rawPassword);
 //	        System.out.println("Encoded Password: " + encodedPassword);
