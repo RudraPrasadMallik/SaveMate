3fdfe010c155b41b91f13dba88765caced401f3e
Rudra Prasad Mallik <117834825+RudraPrasadMallik@users.noreply.github.com>
2025-04-21 19:16:20 +0530

    admin pannel changes added

--- src/main/java/com/savemate/controller/HomeController.java
+++ src/main/java/com/savemate/controller/HomeController.java
@@ -19,7 +19,6 @@ import com.savemate.service.AppSeoService;
 import com.savemate.service.CouponService;
 import com.savemate.service.HomeService;
 
-//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/home")
 public class HomeController {
@@ -35,44 +34,32 @@ public class HomeController {
    
     // ✅ Fetch all advertisements
     @GetMapping("/ads")
- //   @CrossOrigin(origins = "https://savemateapp.onrender.com")
     public List<Advertisement> getAllAds() {
         return   homeService.getAllAds();
     }
 
     // ✅ Fetch all sections
     @GetMapping("/sections")
-  //  @CrossOrigin(origins = "https://savemateapp.onrender.com")
     public List<Section> getAllSections() {
         return homeService.getAllSections();
     }
     
     
-    
     @GetMapping("/coupons")
-  //  @CrossOrigin(origins = "https://savemateapp.onrender.com")
     public List<Coupon> getAllCoupons() {
         return couponService.getAllCoupons();
     }
     
-
-    
 //    Get coupon & Merchant from customers
     @PostMapping("/submitcoupon")
-  //  @CrossOrigin(origins = "https://savemateapp.onrender.com")
     public SubmitCoupon submitCoupon(@RequestBody SubmitCoupon sc) {
 		return homeService.getCouponCust(sc);
     	
     }
     @PostMapping("/submitmerchant")
-  //  @CrossOrigin(origins = "https://savemateapp.onrender.com")
     public SubmitMerchant submitMrechant(@RequestBody SubmitMerchant sm) {
 		return homeService.getMerchantCust(sm);
     	
     }
-    
-    
-    
-
-    
+       
 }
